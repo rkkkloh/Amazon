73ccea829322831cfc876829ad6dfd0d00c24d3e
rkkkloh <ruikangloh@gmail.com>
2024-07-14 16:01:17 +0800

    Update the cart with selected product quantity

--- Lesson/Amazon asset/javascript-amazon-project-main/scripts/amazon.js
+++ Lesson/Amazon asset/javascript-amazon-project-main/scripts/amazon.js
@@ -32,7 +32,7 @@ function renderProductsGrid() {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
