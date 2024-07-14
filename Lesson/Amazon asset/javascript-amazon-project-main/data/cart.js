a33f0593f1c0b0df279a5a1857885e92541a3a9a
rkkkloh <ruikangloh@gmail.com>
2024-07-15 01:03:31 +0800

    Make the webpage interactive by displaying the updated cart details on the page

--- Lesson/Amazon asset/javascript-amazon-project-main/data/cart.js
+++ Lesson/Amazon asset/javascript-amazon-project-main/data/cart.js
@@ -95,6 +95,20 @@ export function calculateCartQuantity() {
   return cartQuantity;
 }
 
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
+
 export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
--- Lesson/Amazon asset/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ Lesson/Amazon asset/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateDeliveryOption} from '../../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -48,10 +48,16 @@ export function renderOrderSummary() {
             <div class="product-quantity
               js-product-quantity-${matchingProduct.id}">
               <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
               </span>
-              <span class="update-quantity-link link-primary">
+              <span class="update-quantity-link link-primary js-update-link"
+              data-product-id="${matchingProduct.id}">
                 Update
+              </span>
+              <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+              <span class="save-quantity-link link-primary js-save-link"
+                data-product-id="${matchingProduct.id}">
+                Save
               </span>
               <span class="delete-quantity-link link-primary js-delete-link
                 js-delete-link-${matchingProduct.id}"
@@ -145,6 +151,50 @@ export function renderOrderSummary() {
     });
 
   updateCartQuantity();
+
+  document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity');
+    });
+  });
+
+  document.querySelectorAll('.js-save-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const quantityInput = document.querySelector(
+        `.js-quantity-input-${productId}`
+      );
+      const newQuantity = Number(quantityInput.value);
+
+      if (newQuantity <= 0 || newQuantity >= 1000) {
+        alert('Quantity must be at least 0 and less than 1000');
+        return;
+      }
+      updateQuantity(productId, newQuantity);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.remove('is-editing-quantity');
+
+      const quantityLabel = document.querySelector(
+        `.js-quantity-label-${productId}`
+      );
+      quantityLabel.innerHTML = newQuantity;
+
+      updateCartQuantity();
+
+      renderPaymentSummary();
+    });
+  });
 }
 
 function updateCartQuantity() {
