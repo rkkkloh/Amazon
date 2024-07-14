641752f0937f1435307893b7b541786286630c77
rkkkloh <ruikangloh@gmail.com>
2024-07-15 02:44:38 +0800

    Move checkout page header to a seperate file

--- Lesson/Amazon asset/javascript-amazon-project-main/scripts/checkout.js
+++ Lesson/Amazon asset/javascript-amazon-project-main/scripts/checkout.js
@@ -1,3 +1,4 @@
+import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
@@ -22,6 +23,7 @@ async function loadPage() {
     console.log('Unexpected error. Please try again later.');
   }
 
+  renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 }
