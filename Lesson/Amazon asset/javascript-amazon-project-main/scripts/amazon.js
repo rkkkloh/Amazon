f83d88c22326bf1ff49828111fc2241301a44f28
rkkkloh <ruikangloh@gmail.com>
2024-07-15 13:29:10 +0800

    Implement Search bar feature by using URL param && Add keyword and case-insensitive feature while filtering

--- Lesson/Amazon asset/javascript-amazon-project-main/scripts/amazon.js
+++ Lesson/Amazon asset/javascript-amazon-project-main/scripts/amazon.js
@@ -7,7 +7,27 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if (keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword ||
+        product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
@@ -83,4 +103,10 @@ function renderProductsGrid() {
         updateCartQuantity();
       });
     });
+
+  document.querySelector('.js-search-button')
+  .addEventListener('click', () => {
+    const search = document.querySelector('.js-search-bar').value;
+    window.location.href = `amazon.html?search=${search}`;
+  });
 }
